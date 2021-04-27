8d589fbf1f70ddd2154fe975d828f4e2c042de39
dnjswnsthd <dnjswnsthd@naver.com>
2021-04-27 18:20:29 +0900

    Fix 버그 수정

--- backend/src/main/java/com/fridge/controller/FridgeController.java
+++ backend/src/main/java/com/fridge/controller/FridgeController.java
@@ -6,12 +6,12 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.fridge.model.Fridge;
 import com.fridge.model.service.FridgeService;
