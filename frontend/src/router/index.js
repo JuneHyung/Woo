9583862817ee6e2e843694cf87dcdd03b2bfddab
조준형 <cjh951114@naver.com>
2021-04-27 17:03:24 +0900

    create S04P31D109-60 : 레시피 목록 페이지 틀잡기.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -13,6 +13,10 @@ import Subscribe from "../views/Subscribe.vue";
 
 import RefAdd from "../views/Refrigerator/RefAdd.vue";
 import RefManage from "../views/Refrigerator/RefManage.vue";
+
+import RecipeList from "../views/Recipe/RecipeList.vue";
+import RecipeDetail from "../views/Recipe/RecipeDetail.vue";
+import RecipeCreate from "../views/Recipe/RecipeCreate.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -55,6 +59,21 @@ const routes = [
     path: "/refmanage",
     name: "RefManage",
     components: { default: RefManage, header: MainHeader },
+  },
+  {
+    path: "/recipeList",
+    name: "RecipeList",
+    components: { default: RecipeList, header: MainHeader },
+  },
+  {
+    path: "/recipeDetail",
+    name: "RecipeDetail",
+    components: { default: RecipeDetail, header: MainHeader },
+  },
+  {
+    path: "/recipeCreate",
+    name: "RecipeCreate",
+    components: { default: RecipeCreate, header: MainHeader },
   },
 ];
 
