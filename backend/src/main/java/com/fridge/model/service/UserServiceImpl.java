864d274af5a9308e0010cfe4196108b48455e529
조석준 <vcxttlvcx@naver.com>
2021-04-27 17:31:02 +0900

    Remove S04P31D109-64 : JWT & 시큐리티 인증을 위한 컨트롤러 및 서비스 수정

--- backend/src/main/java/com/fridge/model/service/UserServiceImpl.java
+++ backend/src/main/java/com/fridge/model/service/UserServiceImpl.java
@@ -1,13 +1,16 @@
 package com.fridge.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.fridge.model.User;
 import com.fridge.model.repository.UserRepository;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 	@Autowired
 	private UserRepository userrepo;
 	@Override
@@ -21,6 +24,12 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public void join(User user) throws Exception {
 		userrepo.save(user);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return userrepo.findByEmail(username)
+				.orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
 	}
 	
 }
