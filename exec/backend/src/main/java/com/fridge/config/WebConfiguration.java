503e7b97c7d6f79c1e3443d27d6f322ad37282a7
조석준 <vcxttlvcx@naver.com>
2021-04-29 21:31:28 +0900

    Fix S04P31D109-96 : SpringSecurity 설정으로 해결 시도

--- exec/backend/src/main/java/com/fridge/config/WebConfiguration.java
+++ exec/backend/src/main/java/com/fridge/config/WebConfiguration.java
@@ -1,13 +1,11 @@
 package com.fridge.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebConfiguration implements WebMvcConfigurer {
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
-	}
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+//	}
 }
