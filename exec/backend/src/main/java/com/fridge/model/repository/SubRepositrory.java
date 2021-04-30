a3a2ccb1b083726f1131c78f1e9957b8c7e10439
seaw7777 <seaw7777@gmail.com>
2021-04-30 16:50:54 +0900

    Fix S04P31D109-69 : 오탈자수정

--- exec/backend/src/main/java/com/fridge/model/repository/SubRepositrory.java
+++
@@ -1,9 +0,0 @@
-package com.fridge.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fridge.model.Sub;
-
-public interface SubRepositrory extends JpaRepository<Sub, Integer> {
-
-}
