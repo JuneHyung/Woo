d9d942b4e0d2b767111c1d0a6e8f19893a11e8a3
조준형 <cjh951114@naver.com>
2021-04-30 17:14:41 +0900

    update S04P31D109-111 : 로그인 로그아웃

--- exec/frontend/src/api/axios.js
+++ exec/frontend/src/api/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 // axios 객체 생성
 export default axios.create({
     
-    baseURL: 'http://localhost:8000/api/', // baseUrl
-    // baseURL: 'https://k4d109.p.ssafy.io/api/', // baseUrl
+    // baseURL: 'http://localhost:8000/api/', // baseUrl
+    baseURL: 'https://k4d109.p.ssafy.io/api/', // baseUrl
     headers: {
         'Content-type': 'application/json',
     },
--- exec/frontend/src/store/index.js
+++ exec/frontend/src/store/index.js
@@ -1,11 +1,44 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { findById } from '@/api/user.js';
+// import jwt_decode from 'jwt-decode';
+
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+    state: {
+    isLogin: false,
+    },
+    
+    mutations: {
+    setIsLogined(state, isLogin) {
+        state.isLogin = isLogin;
+    },
+    
+    logout(state) {
+        state.isLogin = false;
+    }
+    },
+    actions: {
+        async GET_USER_INFO() {
+            await findById( // user.js에 있는 findByid
+                (response) => {
+                    if (response.data.message === 'success') { // succeess가 돌아오면
+                        console.log('가져오기 성공');
+                    } else { // 아니면 유저정보가없음.
+                        console.log('유저 정보 없음!!');
+                    }
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+        },
+    LOGOUT({ commit }) {
+        commit('logout');
+        localStorage.removeItem('X-AUTH-TOKEN');
+    }
+},
+modules: {},
 });
