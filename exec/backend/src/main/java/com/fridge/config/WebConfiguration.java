1715b6285453d8095e739d1f81dd04355a4b9d28
조석준 <vcxttlvcx@naver.com>
2021-05-03 16:50:50 +0900

    Fix S04P31D109-120 : CORS frelight 해결

--- exec/backend/src/main/java/com/fridge/config/WebConfiguration.java
+++ exec/backend/src/main/java/com/fridge/config/WebConfiguration.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+		registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("GET", "POST", "PUT", "DELETE");
 	}
 }
